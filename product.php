440318324547d61768f5c4e9275f31a980cea15b
JulesKoster <juleskoster@hotmail.com>
2018-11-29 11:46:19 +0100

    made product page, changed links

---
+++ product.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Product</title>
+    <meta name="description" content="Roughly 155 characters">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <script src="script.js"></script>
+  </head>
+<body>
+
+ 
+<!-- Nav Start-->
+<nav>
+    <div class="navbar">
+    <a href="index.html">Home</a>
+    <a href="#">Contact</a>
+    <a href="#">Register</a>
+    <a href="#">Login<i class="fa fa-fw fa-user"></i></a>      
+    </div>
+</nav>
+<!-- Nav End-->
+
+<!-- Product innfo Start -->
+<?php
+    include "db_connection.php";
+
+    // $personFirstname = $_GET['firstname'];
+
+    $sql = "SELECT brand, model, color, price, image_url FROM products WHERE productID=1"; 
+    
+    $data = $conn->query($sql);     
+  
+     foreach ($data as $row)
+    {   
+        $htmlOutput = "";         
+        $htmlOutput.= '<div class="product-card">';
+        // $htmlOutput .= '<a href="' . 'personpage.php?firstname=' . $row['firstname'] . '">';
+        $htmlOutput.= '<img src="' . $row['image_url'] . '" alt="' . '" style="width:100%">';
+        // $htmloutput.= '</a>';
+        $htmlOutput.= '<h1>' . $row['brand'] . ' ' . $row['model'] . '</h1>';
+        $htmlOutput.= '<p class="price">' . '<b>&euro; </b>' . $row['price'] . '</p>'; 
+        $htmlOutput.= '<p>' . '<b>Color:</b>' . ' ' . $row['color'] . '</p>'; 
+        $htmlOutput.= '<p>Product info</p>';
+        $htmlOutput.= '<p><button>Add to Cart  <i class="fas fa-cart-plus"></i></button></p>';
+        $htmlOutput.= '</div>';   
+        echo $htmlOutput;
+    }  
+
+    $conn = null;     
+  
+?>
+ 
+
+
+
+
+
+</body>
+</html>  
+
--- register.php
+++ register.php
@@ -7,7 +7,7 @@ $user_email = $_POST["user_email"];
 $user_password = $_POST["user_password"];
 
 
-try {
+//switched of try not to clutter dbbase  try {
     $sql = "INSERT INTO members (uname, upassword, email)
     VALUES ('$user_name', '$user_password','$user_email')";
     // use exec() because no results are returned
@@ -21,8 +21,58 @@ catch(PDOException $e)
 
 $conn = null;
 
-// header("Location: index.php");  
+header("Location: index.php");  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Sign Up Form 2</title>
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="register.css">
+    <!-- <script src="script.js"></script> -->
+  </head>
+  <body>
+    <!-- Content -->
+ 
+<body>
+  
+<div class="formwrap">
+  <form name="register" action="register.php" method="post">
+  
+    <h1>Sign Up</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+
+    <label for="username"><span><b>Username</b></span></label>
+    <input type="text" placeholder="Enter Username" name="user_name" required maxlength="20"> <br> <!-- maxlength 20 because of security-->
+    
+    <label for="email"><span><b>Email</b></span></label>
+    <input type="text" placeholder="Enter Email" name="user_email" required><br>
+    
+    <label for="psw"><span><b>Password</b></span></label>
+    <input type="password" placeholder="Enter Password" name="user_password" required><br>     
+    
+    
+    <!-- Optional, probably use later
+     <label>
+     <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+    </label> -->
+    
+    <!-- <p>By creating an account you agree to our <a href="#" style="color:darkorange">Terms & Privacy</a>.</p> optional -->
+
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
+  </form>
+  
+</div>
+
+
+</body>
+</html>
 
 
 
